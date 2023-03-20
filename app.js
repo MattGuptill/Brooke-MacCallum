3aa54730e548d1303b5c678bc2c00463f4dac2bd
Matthew Guptill <mattguptill@gmail.com>
2023-03-20 11:26:45 -0500

    index finished for review

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// dropdown menu function
+function myFunction() {
+    var x = document.getElementById("mobile-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
