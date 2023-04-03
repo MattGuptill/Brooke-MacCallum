a6879e3ee971098670aff6446fd566d4ab01c2a4
Matthew Guptill <mattguptill@gmail.com>
2023-04-02 20:16:56 -0500

    changes made

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
-// dropdown menu function
-function myFunction() {
-    var x = document.getElementById("mobile-menu");
+// dropdown mobile menu function
+function mobileMenu() {
+    let x = document.getElementById("mobile-menu");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   }
+
+  // pricing menu function
